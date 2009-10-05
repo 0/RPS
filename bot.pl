da1585a40ae0a4952ec9d9a2e50a4fc63b2386c2
user <user>
2009-10-04 23:44:27 -0400

    Amalgamated resulting reverse/ahead calculations in sub throw

--- bot.pl
+++ bot.pl
@@ -107,7 +107,7 @@ sub throw_reset {
 ### Frequency analysis of throws
 #
 sub alg_freq {
-	my ($rev, $ahead, $threshold) = @_;
+	my ($rev, $threshold) = @_;
 	my $max = max ($rev ? @self_count : @opponent_count);
 	my @pool;
 	foreach my $a (ROCK, PAPER, SCISSORS) {
@@ -117,14 +117,7 @@ sub alg_freq {
 		}
 	}
 
-	my $answer = $pool[int (@pool * rand)];
-	if ($ahead == 0) {
-		return will_beat($answer);
-	} elsif ($ahead == 1) {
-		return $answer;
-	} else {
-		return will_lose($answer);
-	}
+	return $pool[int (@pool * rand)];
 }
 
 ### Pattern matching of history
@@ -132,7 +125,7 @@ sub alg_freq {
 sub alg_pattern {
 	return random_throw () if @history <= 0;
 
-	my ($rev, $ahead, $history_distance) = @_;
+	my ($rev, $history_distance) = @_;
 	my $max_history = $history_distance < scalar @history ? $history_distance : scalar @history;
 
 	# load
@@ -155,14 +148,7 @@ sub alg_pattern {
 	$alg_pattern_match_last = $match_last;
 	$alg_pattern_len = $len;
 
-	my $answer = will_beat ($history[$match_last-1]->[$rev ? YOUR : THEIR]);
-	if ($ahead == 0) {
-		return will_beat($answer);
-	} elsif ($ahead == 1) {
-		return $answer;
-	} else {
-		return will_lose($answer);
-	}
+	return will_beat ($history[$match_last-1]->[$rev ? YOUR : THEIR]);
 }
 
 ### When all else fails...
@@ -177,7 +163,7 @@ sub alg_random {
 ###
 
 ### Algorithms hash
-# code		=> reference to sub; args: rev?, n ahead, value
+# code		=> reference to sub; args: rev?, value
 # values	=> array of values to try
 # success	=> foreach value, success rate: [normal, reversed],
 #              and each is [normal, 1 ahead, 2 ahead]
@@ -227,8 +213,13 @@ sub choose {
 #
 sub throw {
 	my ($alg, $val, $rev, $ahd) = @_;
-	my $throw = $algorithms{$alg}->{code}->($rev, $ahd, $algorithms{$alg}->{values}->[$val]);
-	return $throw;
+	my $throw = $algorithms{$alg}->{code}->($rev, $algorithms{$alg}->{values}->[$val]);
+	if ($ahd == 0) {
+		$throw = will_beat($throw);
+	} elsif ($ahd == 2) {
+		$throw = will_lose($throw);
+	}
+	return ($rev ? will_beat($throw) : $throw);
 }
 
 ### Grade how the algorithms would have performed
@@ -242,7 +233,7 @@ sub grade {
 			foreach my $rev (0, 1) {
 				foreach my $j (0..2) {
 					my $r = throw ($a, $i, $rev, $j);
-					$r = will_beat ($r) if $rev;
+#					$r = will_beat ($r) if $rev;
 					if ($r == will_beat ($b)) {
 #print STDERR "$a PLUS cause $r $b\n";
 						++$algorithms{$a}->{success}->[$i]->[$rev]->[$j];
@@ -264,15 +255,7 @@ sub grade {
 ### Determine the next move and return it for output
 #
 sub out {
-	my ($alg, $val, $rev, $ahd) = choose ();
-
-	my $throw = throw ($alg, $val, $rev, $ahd);
-
-	if ($rev) {
-		return will_beat ($throw);
-	} else {
-		return $throw;
-	}
+	return throw (choose ());
 }
 
 ### Record the outcome of the last throw
